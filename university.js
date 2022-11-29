80a0ffc840468be46d1913961d76044967887797
Nina26nina <asma514alim@gmail.com>
2022-11-29 17:54:39 -0500

    pulished university list
    publish university list code

---
+++ university.js
@@ -0,0 +1,5 @@
+$(document).ready(function({
+        $.get("http://universities.hipolabs.com/search?country=Canada&quot; , function(data,status){
+            for (i = 0; i < 3; i++)
+        });
+})
